fed8b53343f2752a7adad92fdd4a50841b1186f6
Michal Smialko <michal.smialko@gmail.com>
2014-12-22 11:38:46 +0100

    Add new empty project.

---
+++ DrugsReminder/DrugsReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrugsReminder
+//
+//  Created by Michal Smialko on 22/12/14.
+//  Copyright (c) 2014 Prismake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
