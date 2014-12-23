5d37a467c34b9ba7832127036784156762cbba44
Michal Smialko <michal.smialko@gmail.com>
2014-12-23 21:18:19 +0100

    Add Core Data database and set it up.

---
+++ DrugsReminder/DrugsReminder/Classes/Model/Drug.swift
@@ -0,0 +1,16 @@
+//
+//  Drug.swift
+//  DrugsReminder
+//
+//  Created by Michal Smialko on 23/12/14.
+//  Copyright (c) 2014 Prismake. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Drug: NSManagedObject {
+
+    @NSManaged var name: String
+    
+}
